36e3e5c513fb2f4cd40190256375999baddd0bcd
Frank-1mbuki <frankimbuki03@gmail.com>
2026-08-16 02:43:47 -0700

    update the attendance thresholds

--- setup_project.sh
+++ setup_project.sh
@@ -11,3 +11,19 @@ touch Helpers/assets.csv
 touch Helpers/config.json
 touch reports/reports.log
 
+#Prompt the user to decide if they want to update the attendance thresholds.
+
+echo "Do you want to update attendance thresholds? (y/n)"
+read UPDATE_CONF
+
+if [ $UPDATE_CONF == "y" ]
+then
+    echo "Enter new warning:"
+    read NEW_WARN
+    echo "Enter new failure:"
+    read NEW_FAIL
+    
+    # Trying to replace the numbers in the config file
+    sed -i s/75/$NEW_WARN/g Helpers/config.json
+    sed -i s/50/$NEW_FAIL/g Helpers/config.json
+fi
