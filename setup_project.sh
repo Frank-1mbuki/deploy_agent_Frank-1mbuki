cb48743809b42ee26ae9a3d4ec0d07c4e412bbcf
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-18 10:29:37 +0400

    Fix prompt formatting in setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash                                                                
 #Project Factory for the Student Attendance Tracker.                               
 
-read -p "What is project name?" DIR_NAME
+read -p "What is project name? " DIR_NAME
 
 #Set up directory structure.                                                       
 mkdir -p attendance_tracker_${DIR_NAME}/Helpers attendance_tracker_${DIR_NAME}/reports
